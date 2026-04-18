b6c0637733e188565d7c2a6b33a7d202c5417c7b
BaronRastignak <baron.rastignak@gmail.com>
2026-04-18 17:41:07 +0300

    Добавлен механизм регистрации входящих файлов и тесты
    Добавлен интерфейс IFileRegistrator и класс FileRegistrator для отслеживания поступающих файлов и сбора их метаданных. Добавлен тестовый проект MSTest для покрытия добавленного класса, включая сценарии повторного поступления того же файла и множественного поступления различных файлов. Обновлён файл солюшена, добавлен проект с тестами.

---
+++ IncomingFileDetector.Service/Service/FileRegistrator.cs
@@ -0,0 +1,28 @@
+﻿using IncomingFileDetector.Service.Model;
+
+namespace IncomingFileDetector.Service;
+
+/// <summary>
+/// Реализует функциональность для регистрации входящих файлов и форматирования их метаданных.
+/// </summary>
+public class FileRegistrator : IFileRegistrator
+{
+    private readonly HashSet<string> _registeredFiles = [];
+    
+    #region Implementation of IFileRegistrator
+
+    /// <inheritdoc />
+    public bool RegisterFile(FileInfo fileInfo, out IncomingFile registeredFile)
+    {
+        registeredFile = new IncomingFile(
+            fileInfo.FullName,
+            DateTimeOffset.UtcNow,
+            fileInfo.CreationTimeUtc,
+            fileInfo.LastWriteTimeUtc,
+            fileInfo.Length
+        );
+        return _registeredFiles.Add(fileInfo.FullName);
+    }
+
+    #endregion
+}
---
+++ IncomingFileDetector.Service/Service/IFileRegistrator.cs
@@ -0,0 +1,30 @@
+﻿using IncomingFileDetector.Service.Model;
+
+namespace IncomingFileDetector.Service;
+
+/// <summary>
+/// Определяет интерфейс для регистрации файлов и получения метаданных о них.
+/// </summary>
+public interface IFileRegistrator
+{
+    /// <summary>
+    /// Регистрирует указанный файл и возвращает объект <see cref="IncomingFile"/>, 
+    /// содержащий метаданные о зарегистрированном файле.
+    /// </summary>
+    /// <param name="fileInfo">Информация о файле, который необходимо зарегистрировать.</param>
+    /// <param name="registeredFile">
+    /// Выходной параметр, содержащий объект <see cref="IncomingFile"/> с метаданными 
+    /// о зарегистрированном файле.
+    /// </param>
+    /// <returns>
+    /// Значение <see langword="true"/>, если файл был успешно зарегистрирован; 
+    /// в противном случае (если файл уже был зарегистрирован ранее) — <see langword="false"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Выбрасывается, если <paramref name="fileInfo"/> равен <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="IOException">
+    /// Выбрасывается, если произошла ошибка при доступе к файлу.
+    /// </exception>
+    bool RegisterFile(FileInfo fileInfo, out IncomingFile registeredFile);
+}
