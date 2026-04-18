82cf0762b8df650a8d054e0abe715a561ce4af1c
BaronRastignak <baron.rastignak@gmail.com>
2026-04-18 16:06:38 +0300

    Добавлен тип IncomingFile (record) и детектор MIME-типов
    Добавлен тип IncomingFile (record) в неймспейсе Model для хранения метаданных входящего файла, включая MIME-тип. Подключен NuGet-пакет MimeTypes для детектирования MIME-типа, исходя из расширения файла.

---
+++ IncomingFileDetector.Service/Model/IncomingFile.cs
@@ -0,0 +1,35 @@
+﻿namespace IncomingFileDetector.Service.Model;
+
+/// <summary>
+/// Представляет входящий файл с метаданными, такими как полный путь, время регистрации, 
+/// время создания, время изменения и размер. Предоставляет дополнительные свойства 
+/// для получения имени файла и MIME-типа.
+/// </summary>
+/// <param name="FullPath">Полный путь к файлу.</param>
+/// <param name="RegisteredAt">Время регистрации входящего файла.</param>
+/// <param name="CreatedAt">Время создания файла.</param>
+/// <param name="ModifiedAt">Время последнего изменения файла.</param>
+/// <param name="Length">Размер файла в байтах.</param>
+public record IncomingFile(
+    string FullPath,
+    DateTimeOffset RegisteredAt,
+    DateTimeOffset CreatedAt,
+    DateTimeOffset ModifiedAt,
+    long Length)
+{
+    /// <summary>
+    /// Возвращает имя файла, включая его расширение, из полного пути.
+    /// </summary>
+    /// <value>
+    /// Строка (<see cref="string"/>), представляющая имя файла, извлечённое из свойства <see cref="FullPath"/>.
+    /// </value>
+    public string FileName => Path.GetFileName(FullPath);
+    
+    /// <summary>
+    /// Возвращает MIME-тип файла на основе его расширения.
+    /// </summary>
+    /// <value>
+    /// Строка (<see cref="string"/>), представляющая MIME-тип файла.
+    /// </value>
+    public string MimeType => MimeTypes.GetMimeType(FullPath);
+}
